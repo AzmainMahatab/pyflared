bde1f5bfe5714a47c871a83c2c6c86c3387304cf
Azmain <azmainmahatab012@gmail.com>
2025-11-29 22:09:09 +0600

    partial asyncio migration timestamp 5

---
+++ src/pyflared/tunnel3.py
@@ -0,0 +1,11 @@
+class TunnelManager:
+    def __init__(self, token: str):
+        self.token = token
+
+    async def accounts(self):
+        return
+
+    async def zones(self):
+        return
+
+    async def
